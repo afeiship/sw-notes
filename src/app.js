bf55c84ca52f5f4ef09f3ade6227f8dfaf97676b
afeiship <1290657123@qq.com>
2019-06-17 22:01:20 +0800

    add: failed cache files

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// registet sw:
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js', { scope: '/' })
+    .then((registration) => console.log('ServiceWorker 注册成功！作用域为: ', registration.scope))
+    .catch((err) => console.log('ServiceWorker 注册失败: ', err));
+}
+
+
+console.log('hello, sw1');
---
+++ src/sw.js
@@ -0,0 +1,52 @@
+// 用于标注创建的缓存，也可以根据它来建立版本规范
+const CACHE_NAME = 'app_cache_v1.0.0';
+// 列举要默认缓存的静态资源，一般用于离线使用
+const urlsToCache = ['/offline.html', '/app.js', '/'];
+
+// self 为当前 scope 内的上下文
+self.addEventListener('install', (event) => {
+  // event.waitUtil 用于在安装成功之前执行一些预装逻辑
+  // 但是建议只做一些轻量级和非常重要资源的缓存，减少安装失败的概率
+  // 安装成功后 ServiceWorker 状态会从 installing 变为 installed
+  event.waitUntil(
+    // 使用 cache API 打开指定的 cache 文件
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log(cache);
+      // 添加要缓存的资源列表
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) =>
+  event.waitUntil(
+    Promise.all([
+      // 更新客户端
+      clients.claim(),
+      // 清理旧版本
+      caches.keys().then((cacheList) =>
+        Promise.all(
+          cacheList.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              caches.delete(cacheName);
+            }
+          })
+        )
+      )
+    ])
+  )
+);
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((hit) => {
+      // 返回缓存中命中的文件
+      if (hit) {
+        console.log('hit!');
+        return hit;
+      } else {
+        console.log('no cache!');
+      }
+    })
+  );
+});
